5f0714482f2d2789e61eccc58e63f45585cd2d5f
Emanuele Cardelli <emanuele.cardelli@glovoapp.com>
2022-02-19 17:03:09 +0100

    game service - alternative implementations

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -33,6 +33,29 @@ const internalPlay = (userMove: Move, computerMove: Move): T.Task<Result>  => {
     }
 }
 
+export const createGameServiceImperative = (repo: GameRepository) => ({
+  play: (userMove: Move): Promise<Result> => {
+    const computerMove = randomMove()
+    const resultPromise: Promise<Result> = internalPlay(userMove, computerMove)() // side effect!! here the promise start running
+    return resultPromise
+      .then(result => {
+        const response: PlayResponse = {userMove, computerMove, result}
+        return repo
+          .insert(response)()
+          .then(response => {
+              if(response._tag == 'Left') {
+                throw response.left
+              }
+              else {
+                return response.right.result
+              }
+          })
+
+      })        
+  },
+  lastPlay: () => repo.getLast,
+});
+
 const internalPlayMapped = (userMove: Move, computerMove: Move): T.Task<PlayResponse> => 
   T.map((result: Result) => ({userMove, computerMove, result}))(internalPlay(userMove, computerMove))
   //internalPlay(userMove, computerMove).map((result) => ...) most common, but the other one is giving you code suggestion when used inside a pipe
@@ -43,6 +66,19 @@ export const createGameService = (repo: GameRepository): GameService => ({
       computerMove => internalPlayMapped(userMove, computerMove),      
       T.chain(repo.insert), // can chain task and taskEither because taskEither is a subtype of task
       TE.map(r => r.result) // mapping of taskeither applies ONLY if the inner result is right, you can map only the error with .mapLeft or both results with .bimap
+    ),
+  lastPlay: () => repo.getLast,
+});
+
+//don't use an intermediate map, but to me it's harder to understand, it's miming Haskell do syntax (or scala's flatMap)
+export const createGameServiceBind = (repo: GameRepository): GameService => ({
+  play: (userMove: Move): TE.TaskEither<RPSError, Result> => pipe(
+      T.Do,
+      T.bind('computerMove', () => T.of(randomMove())),
+      T.bind('result', ({computerMove}) => internalPlay(userMove, computerMove)),
+      T.map(({computerMove, result}) => ({userMove, computerMove, result})),
+      T.chain(repo.insert),
+      TE.map(r => r.result)
     ),
   lastPlay: () => repo.getLast,
 });
